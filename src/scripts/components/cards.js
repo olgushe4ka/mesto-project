3fc19abd25f0430a536e8f9d46addccedd59dd2e
olgushe4ka <olgushe4ka@gmail.com>
2022-07-08 18:30:36 +0300

    gh pages, likes added

--- src/scripts/components/cards.js
+++ src/scripts/components/cards.js
@@ -2,18 +2,28 @@
 
 import { openPopup } from '../components/modal.js';
 import { myOwnerId, photoPopup } from '../components/constants.js';
-import { deleteCard } from './api.js';
+import { deleteCard, deleteLike, putLike } from './api.js';
 
-function like(evt) {
-  evt.target.classList.toggle('photo-grid__heart_black');
-};
+function like(evt, cardId) {
+  const toogleLikePromise = evt.target.classList.contains('photo-grid__heart_black') ?
+    deleteLike(cardId) :
+    putLike(cardId);
+  toogleLikePromise.then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+  }).then(card => {
+    evt.target.parentElement.querySelector('.photo-grid__counter').textContent = card.likes.length;
+    evt.target.classList.toggle('photo-grid__heart_black');
+  });
+}
 
 function removeParentItem(evt) {
   evt.target.parentElement.remove();
 };
 
 
-export function renderCard(imageName, imageLink, imageLike, imageAlt, hideDeleteButton, imageId) {
+export function renderCard(imageName, imageLink, imageLike, imageAlt, hideDeleteButton, imageId, myLikeExists) {
   const photoTemplate = document.querySelector('#photo-grid').content;
   const photoItem = photoTemplate.querySelector('.photo-grid__item').cloneNode(true);
   const photoElement = photoItem.querySelector('.photo-grid__photo');
@@ -23,7 +33,15 @@ export function renderCard(imageName, imageLink, imageLike, imageAlt, hideDelete
   photoItem.querySelector('.photo-grid__counter').textContent = imageLike.length;
   photoItem.querySelector('.photo-grid__photo').alt = imageAlt;
 
-  photoItem.querySelector('.photo-grid__heart').addEventListener('click', like);
+  if (myLikeExists) {
+    photoItem.querySelector('.photo-grid__heart').classList.add('photo-grid__heart_black');
+  }
+
+  photoItem.querySelector('.photo-grid__heart').addEventListener('click', (evt) => {
+    like(evt, imageId);
+  });
+
+
   if (hideDeleteButton) {
     photoItem.querySelector('.photo-grid__delite-button').remove();
   } else {
@@ -52,7 +70,8 @@ export function renderCard(imageName, imageLink, imageLike, imageAlt, hideDelete
 
 export function addCard(card, container, addToTheBeginning) {
   const hideDeleteButton = card.owner._id !== myOwnerId;
-  const renderedCard = renderCard(card.name, card.link, card.likes, undefined, hideDeleteButton, card._id);
+  const myLikeExists = card.likes.find(like => like._id === myOwnerId) !== undefined;
+  const renderedCard = renderCard(card.name, card.link, card.likes, undefined, hideDeleteButton, card._id, myLikeExists);
   if (addToTheBeginning) {
     container.prepend(renderedCard);
   } else {
