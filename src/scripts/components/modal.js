c0e4960818360ba0c3873260a25e004b69e7fd16
olgushe4ka <olgushe4ka@gmail.com>
2022-07-23 12:06:58 +0300

    bug fixed (inputs now invisible after closing)

--- src/scripts/components/modal.js
+++ src/scripts/components/modal.js
@@ -2,13 +2,14 @@ const ESC_CODE = "Escape";
 
 //открытие попапов
 export function openPopup(popupElement) {
+  document.querySelectorAll('.form__set').forEach((item) => { item.style.display = "block"; });
   popupElement.classList.add('popup_opened');
-
   document.addEventListener('keydown', closeByEsc);
 }
 
 //закрытие попапов
 export function closePopup(popupElement) {
+  document.querySelectorAll('.form__set').forEach((item) => { item.style.display = "none"; });
   popupElement.classList.remove('popup_opened');
 
   document.removeEventListener('keydown', closeByEsc);
